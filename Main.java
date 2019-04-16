a88f7e060384c0903802eb56f2a94f9b2516aae6
supersonico <saita.0521@gmail.com>
2019-04-16 16:06:07 +0900

    modified: Main.java -> Scanner scan

--- Main.java
+++ Main.java
@@ -1,9 +1,19 @@
 package p127;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		scan.useDelimiter(" |\n");
+		
+		String _str[][] = new String[2][5];
+		int _num[][] = new int[2][5];
+		
+		_str[0] = scan.nextLine().split(" ");
+		_str[1] = scan.nextLine().split(" ");
+		scan.close();
 
 	}
 
